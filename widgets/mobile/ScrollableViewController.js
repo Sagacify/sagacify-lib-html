f54512ca6d7bcc5cf69bbf9c13d615c12324f738
fbeuvens <francois.beuven>
2012-12-01 16:11:31 +0100

    scrollable viewe

--- widgets/mobile/ScrollableViewController.js
+++ widgets/mobile/ScrollableViewController.js
@@ -1,20 +1,21 @@
 define([
 	'dojo/_base/declare', 
-	'saga/widgets/mobile/ViewController',
+	'saga/widgets/_Widget',
 	'dojox/mobile/ScrollableView',
 	'dojo/dom-construct'], 
 	
-	function(declare, ViewController, ScrollableView, domConstruct) {
+	function(declare, _Widget, ScrollableView, domConstruct) {
 	
-	return declare('saga.ScrollableViewController', [ViewController, ScrollableView], {
+	return declare('saga.ScrollableViewController', [ScrollableView], {
 		
 		consructor: function(args) {
 			
 		},
 		
 		postCreate: function() {
-			this.inherited(arguments);
-			domConstruct.place(this.domNode.children[0], this.domNode.children[1]);
+			//this.inherited(arguments);
+			//domConstruct.place(this.domNode.children[0], this.domNode.children[1]);
+			domConstruct.create("div", {style:"width:1440px;height:1000px;background:blue;"}, this.containerNode);
 		}
 		
 	});
