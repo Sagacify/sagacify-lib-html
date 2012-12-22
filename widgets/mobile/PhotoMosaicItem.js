7b3d46d168817b57b8417642f7561ff106f4a745
fbeuvens <francois.beuven>
2012-12-22 19:17:56 +0100

    photo

---
+++ widgets/mobile/PhotoMosaicItem.js
@@ -0,0 +1,29 @@
+define([
+	'dojo/_base/declare', 
+	'saga/widgets/_Widget',
+	'dojo/text!./templates/PhotoMosaicItem.html',
+	'dojo/dom-construct',
+	'dojo/on'], 
+	
+	function(declare, _Widget, template, domConstruct, on) {
+	
+	return declare('saga.PhotoMosaicItem', [_Widget], {
+		
+		templateString: template,
+		
+		src: null,
+		
+		constructor: function(args) {
+
+		},
+		
+		postCreate: function(){
+			this.inherited(arguments);	
+        	on(this.linkNode, "click", function(evt){
+        		evt.preventDefault();
+        	});
+        	this.imgNode.src = this.src;
+		},
+		
+	});
+});
---
+++ widgets/mobile/PhotoMosaicViewController.js
@@ -0,0 +1,36 @@
+define([
+	'dojo/_base/declare', 
+	'saga/widgets/mobile/ScrollableViewController',
+	'dojo/text!./templates/PhotoMosaicViewController.html',
+	'saga/widgets/mobile/PhotoMosaicItem',
+	'saga/widgets/mobile/PhotoViewController',
+	'dojo/dom-construct',
+	'dojo/on'], 
+	
+	function(declare, ScrollableViewController, template, PhotoMosaicItem, PhotoViewController, domConstruct, on) {
+	
+	return declare('saga.PhotoMosaicViewController', [ScrollableViewController], {
+		
+		templateString: template,
+		
+		images: null,
+		
+		constructor: function(args) {
+
+		},
+		
+		postCreate: function(){
+			this.inherited(arguments);	
+			var me = this;
+        	dojo.forEach(this.images, function(image, i){
+        		var photoMosaicItem = new PhotoMosaicItem({src:image.src?image.src:image});
+        		photoMosaicItem.placeAt(me.photoContainerNode);
+        		on(photoMosaicItem.domNode, "click", function(evt){
+        			var photoViewController = new PhotoViewController({frame:me.frame, images:me.images, indexToShow:i});
+        			me.navigationController.pushViewController(photoViewController);
+        		});
+        	});
+		},
+		
+	});
+});
