7667b50ae01d7fa4a018e98229f513621f81a41f
fbeuvens <francois.beuven>
2012-12-26 18:52:35 +0100

    detect

--- utils/Utils.js
+++ utils/Utils.js
@@ -19,5 +19,26 @@ define([
 			return support;
 		};
 		
+		saga.Utils.detectOS = function(){
+			if(navigator.userAgent.match(/iPhone|iPad|iPod/i))
+				return "iOS";
+			if(navigator.userAgent.match(/Android/i))
+				return "Android";
+			if(navigator.userAgent.match(/BlackBerry/i))
+				return "BlackBerry";
+			if(navigator.userAgent.match(/Opera Mini/i))
+				return "Opera";
+			if(navigator.userAgent.match(/IEMobile/i))
+				return "IE";
+			return null;
+		};
+		
+		saga.Utils.detectWebOrNative = function(){
+			if(document.URL.indexOf('http://') == -1 && document.URL.indexOf('https://') == -1)
+				return "Native";
+			else
+				return "Web";
+		};
+		
 		return saga.Utils;
 });
