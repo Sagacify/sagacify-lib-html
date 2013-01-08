8df55d37adb8d4e5564e8d8021cc60adf9c2dacc
fbeuvens <francois.beuven>
2013-01-08 22:31:21 +0100

    numpad

--- widgets/mobile/ApplicationViewController.js
+++ widgets/mobile/ApplicationViewController.js
@@ -2,14 +2,17 @@ define([
 	'dojo/_base/declare', 
 	'saga/widgets/mobile/ViewController',
 	'saga/widgets/mobile/Alert',
+	'saga/widgets/mobile/NumPad',
 	'dojo/dom-construct',
 	'dojo/on'], 
 	
-	function(declare, ViewController, Alert, domConstruct, on) {
+	function(declare, ViewController, Alert, NumPad, domConstruct, on) {
 	
 	return declare('saga.ApplicationViewController', [ViewController], {
 		
 		alert: null,
+		
+		numPad: null,
 		
 		mask: null,	
 		
@@ -46,7 +49,38 @@ define([
 			$('.modalView').fadeTo(500, 0, function(){
 				me.alert.domNode.style.display = "none";
 			});
-		}
+		},
+		
+		presentNumPad: function(){
+			if(!this.numPad){
+				this.numPad = new NumPad();
+				this.numPad.domNode.style.top = this.domNode.clientHeight+"px";
+				this.numPad.domNode.style.left = "0px";
+				this.numPad.domNode.style.zIndex = "2";
+				this.numPad.domNode.style.position = "absolute";
+				this.numPad.placeAt(this.domNode);
+				this.numPadVisible = false;
+				var me = this;
+				this.numPad.on("afterTransitionOut", function(){
+					if(!me.numPadVisible){
+						me.numPad.domNode.style.display = "";
+						me.numPad.domNode.style.top = (me.domNode.clientHeight-me.numPad.domNode.clientHeight)+"px";
+						me.numPad.domNode.style.left = "0px";
+						me.numPadVisible = true;
+					}
+					else{
+						me.numPad.domNode.style.top = me.domNode.clientHeight+"px";
+						me.numPadVisible = false;
+					}
+				});
+			}
+			this.numPad.domNode.style.display = "";
+			this.numPad.performTransition(null, 1, "revealv", null);
+		},
+		
+		dismissNumPad: function(){
+			this.numPad.performTransition(null, -1, "revealv", null);
+		},
 
 	});
 });
---
+++ widgets/mobile/NumPad.js
@@ -0,0 +1,57 @@
+define([
+	'dojo/_base/declare', 
+	'saga/widgets/_Widget',
+	'dojox/mobile/View',
+	'dojo/text!./templates/NumPad.html',
+	'dojo/_base/config',
+	'dojo/on',
+	'dojo/dom-class'], 
+	
+	function(declare, _Widget, View, template, config, on, domClass) {
+	
+	return declare('saga.NumPad', [_Widget, View], {
+		
+		templateString: template,
+				
+		constructor: function(args) {
+			
+		},	
+		
+		postCreate: function() {
+			this.inherited(arguments);
+			//workaround to bind webkit animations (that should work in 'View' class but does not)
+			this._animEndHandle = this.connect(this.domNode, "webkitAnimationEnd", "onAnimationEnd");
+			this._animStartHandle = this.connect(this.domNode, "webkitAnimationStart", "onAnimationStart");
+			if(!config['mblCSS3Transition']){
+				this._transEndHandle = this.connect(this.domNode, "webkitTransitionEnd", "onAnimationEnd");
+			}
+			var me = this;
+			dojo.forEach(this.domNode.children, function(li, i){
+				var text = li.children[0].innerHTML;
+				on(li, "click", function(evt){
+					evt.preventDefault();
+					me.onKeySelected.apply(me, [text]);
+					if(text == "OK")
+						Window.dismissNumPad();
+				});
+				on(li, "mousedown", function(evt){
+					domClass.add(li, "selected");					
+				});
+				on(li, "mouseup", function(evt){
+					domClass.remove(li, "selected");					
+				});
+				on(li, "touchstart", function(evt){
+					domClass.add(li, "selected");					
+				});
+				on(li, "touchend", function(evt){
+					domClass.remove(li, "selected");					
+				});
+			});
+		},
+		
+		onKeySelected: function(){
+			
+		}
+		
+	});
+});
