1e95a15bb6cc785fac4f119c9665201d0a17320a
Yvan de Ghellinck <yvan@ghellinck.be>
2013-04-29 14:43:57 +0200

    jklm

--- stores/JsonStore.js
+++ stores/JsonStore.js
@@ -124,7 +124,6 @@ define([
 					deferred.resolve(data);
 				},
 				error: function(error){
-					debugger;
 					if(error.response.status == 401){
 						if (target.endsWith("/auth/local/login")) {
 							me.loginFail();
