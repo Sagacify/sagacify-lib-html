6daf9db8a79d1988447750fa0c01d1777d76cf5f
Yvan de Ghellinck <yvan@ghellinck.be>
2013-05-15 09:33:52 +0200

    jkl

--- stores/JsonStore.js
+++ stores/JsonStore.js
@@ -8,35 +8,31 @@ define([
 	declare("saga.JsonStore", null, {
 		
 		
-		// summary:
-		//		This is a basic store for RESTful communicating with a server through JSON
-		//		formatted data. It implements dojo.store.api.Store.
-		// params:
-		// @args : {'authON':true}
-	
-		constructor: function(args){
-			
-			//this.authON = args.authON;
-			//this.authON = true;
-		},
-		
-		// Header
-		// accepts: String
-		//		Defines the Accept header to use on HTTP requests
 		accepts: "application/javascript, application/json", 
 		
-		// authorization string
-		// 
-
-	
-		bearerString: "bearer "+localStorage.access_token,
+		bearerString: function(){
+			return "bearer "+localStorage.access_token;
+		},
 		
 		
-
-		get: function(target, options, query){
+		configureHeader: function(options, removeAuth){
 			var headers = options || {};
 			headers.Accept = this.accepts;
-			headers.Authorization = "bearer "+localStorage.access_token;
+			if (!removeAuth) {
+				headers.Authorization = this.bearerString();	
+			};
+
+
+			headers["Content-Type"] = "application/json";
+			if (options) {
+				headers["If-Match"] = options.overwrite === true ? "*" : null,
+				headers["If-None-Match"] = options.overwrite === false ? "*" : null				
+			};
+			
+			return headers;
+		},
+
+		configureUrlWithDict: function(target, query){
 			if(query instanceof Object){
 				var queryString = "?";
 				for (queryKey in query){
@@ -44,233 +40,97 @@ define([
 						queryString += "&";
 					queryString += queryKey+"="+query[queryKey];
 				}
-				query = queryString;
 			}
-			if (debugMode) {
-				console.log(this.bearerString);
-				console.log("GET "+target + (query || ""));
-			};	
+			return target + (queryString || "");
+		},
+
+		configureRequestContent: function(query, headers, deferred, data){
+			var request = {};
+				request.headers = headers;
+				request.url = query;
+				request.handleAs = "json";
+				request.preventCache = true;
+				request.headers = headers;
+				request.postData=json.toJson((data || {}));
+				request.load= this.onLoadFunction(deferred);
+			return request;
+		},
+
+		onLoadFunction: function(deferred){
+			return function(data){
+				if (DebugMode) {
+					console.log(data);
+				}
+				deferred.resolve(data);
+			}
+		}, 
+
+		onError: function(deferred){
+			return function(error){
+				debugger;
+				deferred.reject(error);
+			}
+		},
+
+
+		handlingRelog: function(deferred, afterRelog){
+			var me = this;
+			return function(error){
+					if(error.response.status == 401){
+						me.login(afterRelog, function(error){
+							me.loginFail();
+						});
+					} else {
+						me.onError(deferred)(error);
+					};
+				};
+		},
+
+		executeRequest: function(httpMethod, target, headersOptions, queryDict, data, removeAuth, disableRelog){
 			var me = this;
 			var deferred = new Deferred();
-			var xhrDeferred = xhr("GET", {
-				url:target + (query || ""),
-				content: {test:"ok"},
-				postData: json.toJson({test:"ok"}),
-				data: json.toJson({test:"ok"}),
-				handleAs: "json",
-				preventCache: true,
-				headers: headers,
-				load: function(data){
-					if (debugMode) {
-						console.log(data);
-					}
-					deferred.resolve(data);
-				},
-				error: function(error){
-					if(error.response.status == 401){
-						me.login(function(data){
-							headers.Authorization = "bearer "+localStorage.access_token;
-							xhr("GET", {
-								url:target + (query || ""),
-								handleAs: "json",
-								preventCache: true,
-								headers: headers,
-								load: function(data){
-									if (debugMode) {
-										console.log(data);
-									}
-									deferred.resolve(data);
-								},
-								error: function(error){
-									deferred.reject(error);
-								}
-							});
-						}, function(error){
-							me.loginFail();
-							deferred.reject(error);
-						});
-					}
-				}
-			});
-			deferred.ioArgs = xhrDeferred.ioArgs;
-			return deferred;
-		},
-		
-		post: function(target, object, options, removeAuth){
-			var me = this;
-			options = options || {};
-			
-			var headers = options || {};
-			headers.Accept = this.accepts;
-			if(!removeAuth)
-				headers.Authorization = "bearer "+localStorage.access_token;
-			headers["Content-Type"] = "application/json";
-			headers["If-Match"] = options.overwrite === true ? "*" : null,
-			headers["If-None-Match"] = options.overwrite === false ? "*" : null
 
-			if (debugMode) {
-				console.log("POST "+target);
-				console.log(object);
-			};
-			
-			var deferred = new Deferred();
-			xhr.post({
-				url: target,
-				postData: json.toJson(object),
-				handleAs: "json",
-				preventCache: true,
-				headers: headers,
-				load: function(data){
-					if (debugMode) {
-						console.log(data);
-					}
-					deferred.resolve(data);
-				},
-				error: function(error){
-					if(error.response.status == 401){
-						if (target.endsWith("/auth/local/login")) {
-							me.loginFail();
-							deferred.reject(error);
-							return;
-						}
-						me.login(function(loginData){
-							headers.Authorization = "bearer "+localStorage.access_token;
-							xhr("POST", {
-								url:target,
-								postData: json.toJson(object),
-								handleAs: "json",
-								preventCache: true,
-								headers: headers,
-								load: function(data){
-									//debugger;
-									deferred.resolve(data);
-								},
-								error: function(error){
-									//debugger;
-									deferred.reject(error);
-								}
-							});
-						}, function(loginError){
-							me.loginFail();
-							deferred.reject(loginError);
-						});
-					}
-				}
-			});
-			return deferred;
-		},
-		
-		put: function(target, object, options){
-			var me = this;
-			options = options || {};
-			
-			var headers = options || {};
-			headers.Accept = this.accepts;
-			headers.Authorization = "bearer "+localStorage.access_token;
-			headers["Content-Type"] = "application/json";
-			headers["If-Match"] = options.overwrite === true ? "*" : null,
-			headers["If-None-Match"] = options.overwrite === false ? "*" : null
+			var headers = this.configureHeader(headersOptions, removeAuth);
 
-			if (debugMode) {
-				console.log("PUT "+target);
-				console.log(object);
+			var query = this.configureUrlWithDict(target, queryDict);
+			var request = this.configureRequestContent(query, headers, deferred, data);
+
+			if (disableRelog) {
+				request.error = this.onError(deferred);
+			} else {
+				request.error = this.handlingRelog(deferred, function(){
+					var headers = me.configureHeader(headersOptions, removeAuth);
+					var request = me.configureRequestContent(query, headers, deferred);
+					request.error = me.onError(deferred);
+					xhr(httpMethod, request);
+				});
 			}
 
-			var deferred = new Deferred();
-			xhr.put({
-				url: target,
-				putData: json.toJson(object),
-				handleAs: "json",
-				preventCache: true,
-				headers:headers,
-				load: function(data){
-					deferred.resolve(data);
-				},
-				error: function(error){
-					if(error.response.status == 401){
-						me.login(function(loginData){
-							headers.Authorization = "bearer "+localStorage.access_token;
-							xhr("PUT", {
-								url:target,
-								putData: json.toJson(object),
-								handleAs: "json",
-								preventCache: true,
-								headers: headers,
-								load: function(data){
-									deferred.resolve(data);
-								},
-								error: function(error){
-									deferred.reject(error);
-								}
-							});
-						}, function(loginError){
-							me.loginFail();
-							deferred.reject(loginError);
-						});
-					}
-				}
-			});
+			xhr(httpMethod, request);
+
 			return deferred;
+		},
+
+		get: function(target, options, queryDict, removeAuth){
+			return this.executeRequest("GET", target, options, queryDict, null, removeAuth);
+		},
+		
+		post: function(target, object, options, removeAuth, disableRelog){
+			return 	this.executeRequest("POST", target, options, null, object, removeAuth, disableRelog);
+		},
+		
+		put: function(target, object, options, removeAuth){
+			return this.executeRequest("PUT", target, options, null, object, removeAuth);
+
 		},
 		
 		
-		remove: function(target, object, options){
-			var me = this;
-			options = options || {};
-
-			var headers = options || {};
-			headers.Accept = this.accepts;
-			headers.Authorization = "bearer "+localStorage.access_token;
-			headers["Content-Type"] = "application/json";
-			headers["If-Match"] = options.overwrite === true ? "*" : null,
-			headers["If-None-Match"] = options.overwrite === false ? "*" : null
-
-			if (debugMode) {
-				console.log("DELETE "+target);
-			}
-			var deferred = new Deferred();
-			dojo.xhrDelete({
-				url:target,
-				data: object,
-				handleAs: "json",
-				preventCache: true,
-				headers:headers,
-				load: function(data){
-					if (debugMode) {
-						console.log("del ok");
-					}
-					deferred.resolve(data);
-				},
-				error: function(error){
-					if(error.response.status == 401){
-						me.login(function(loginData){
-							headers.Authorization = "bearer "+localStorage.access_token;
-							xhr("DELETE", {
-								url:target,
-								handleAs: "json",
-								preventCache: true,
-								headers: headers,
-								load: function(data){
-									deferred.resolve(data);
-								},
-								error: function(error){
-									deferred.reject(error);
-								}
-							});
-						}, function(loginError){
-							deferred.reject(loginError);
-						});
-					}
-					else{
-						me.loginFail();
-						deferred.reject(error);
-					}
-				}
-			});
-			return deferred;
+		remove: function(target, object, options, removeAuth){
+			return this.executeRequest("DELETE", target, options, null, object, removeAuth);
 		},
 		
 		login: function(success, failure){
+
 		},
 		
 		loginFail: function(){
