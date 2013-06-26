1b522e8925dfd96c0594734e870b0b1f2a5e0a64
mickael van der beek <kollektiv@kollektiv.local>
2013-06-26 18:57:31 +0200

    merge mikl branch

---
+++ widgets/tuto/Help.js
@@ -0,0 +1,47 @@
+define([
+	'dojo/_base/declare',
+	'dojo/dom-attr'
+], function(declare, domAttr) {
+	return declare('saga.Help', null, {
+
+		linkedTutorials: null,
+
+		tutorialDomElement: null,
+
+		postCreate: function() {
+			this.inherited(arguments);
+			if(this.linkedTutorials && Object.keys(this.linkedTutorials).length) {
+				var me = this;
+				this.getTutorials(function(item) {
+					NotificationCenterHelp.register(me, tutorialName);
+				});
+			}
+			//NotificationCenterHelp.register(this, 'createNode');
+		},
+
+		activate: function(tutorialName, i) {
+			for(var i = 0; i < this.linkedTutorials[tutorialName].length; i++) {
+				domAttr.set(this.linkedTutorials[tutorialName][i][0], 'data-intro', this.generateMessage(tutorialName, i));
+				domAttr.set(this.linkedTutorials[tutorialName][i][0], 'data-step', i);
+			}
+			/*domAttr.set(this.domNode, 'data-intro', this.generateMessage(tutorialName));
+			//domAttr.set(this.domNode, 'data-position', this.generateMessage());
+			domAttr.set(this.domNode, 'data-step', i);*/
+		},
+
+		generateMessage: function(tutorialName, i) {
+			return this.linkedTutorials[tutorialName][i][1];
+		},
+
+		/*addTutorials: function(tutorials) {
+			this.linkedTutorials = tutorials;
+		},*/
+
+		getTutorials: function(callback) {
+			for(var tutorialName in this.linkedTutorials) {
+				callback(tutorialName);
+			}
+		}
+
+	});
+});
---
+++ widgets/tuto/NotificationCenterHelp.js
@@ -0,0 +1,33 @@
+define([
+	'dojo/_base/declare'
+], function(declare) {
+	return declare('saga.NotificationCenterHelp', null, {
+	
+		tutorials: null,
+
+		constructor: function(args) {
+			this.inherited(arguments);
+			this.tutorials = {};
+		},
+
+		activate: function(tutorialName) {
+			//this.tutorials = [];
+			//debugger;
+		},
+
+		register: function(widget, tutorialName) {
+			if(!this.tutorials[tutorialName]) {
+				this.tutorials[tutorialName] = [];
+			}
+			this.tutorials[tutorialName].push(widget);
+		}, 
+
+		getTutorialWidgets: function(tutorialName) {
+			alert('+1');
+			if(!this.tutorials[tutorialName]) {
+				this.tutorials[tutorialName] = [];
+			}
+			return this.tutorials[tutorialName];
+		}
+	});
+});
