eee3ffaa28cf1613cb3d8ad46aa14886b172a80a
mickael van der beek <kollektiv@kollektiv.local>
2013-12-04 15:20:21 +0100

    bugfixes

--- router/Router.js
+++ router/Router.js
@@ -13,7 +13,7 @@ define(['backbone', 'backbone.marionette'], function(Backbone, Marionette){
 		},
 
 		handleSagaRoutes: function(){
-			for(var route in this.sagaRoutes){
+			for(var route in this.sagaRoutes) {
 				var specRoute = this.sagaRoutes[route];
 				if(route.contains(" \@ ")){
 					var splitRoute = route.split(" \@ ");
