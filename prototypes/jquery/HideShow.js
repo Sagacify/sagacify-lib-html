8c9d274e254713121360253a27793329e4b9ad22
mickael van der beek <kollektiv@kollektiv.local>
2013-12-09 13:23:50 +0100

    libs

--- prototypes/jquery/HideShow.js
+++ prototypes/jquery/HideShow.js
@@ -1,11 +1,6 @@
-// var _hide = $.fn.hide;
-// $.fn.hide = function(speed, callback) {
-// 	$(this).trigger('jQHide');
-// 	return _hide.apply(this,arguments);
-// };
-
 var _show = $.fn.show;
 $.fn.show = function(speed, callback) {
+	var chain = _show.apply(this, arguments);
 	$(this).trigger('jQShow');
-	return _show.apply(this,arguments);
+	return chain;
 };
