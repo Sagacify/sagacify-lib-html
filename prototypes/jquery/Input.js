6f8d701e5c67794a6edabe81f1201411807252ec
mickael van der beek <kollektiv@kollektiv.local>
2013-12-10 01:30:30 +0100

    bugfixes

--- prototypes/jquery/Input.js
+++ prototypes/jquery/Input.js
@@ -1,16 +1,13 @@
 $.fn.sgRenderImage = function(img){
-	if(!this.is('input')||this.attr('type')!='file'){
-		return;
+	if(this.is('input') && (this.attr('type') !== 'file')) {
+		this.change(function (evt) {
+			if(this.files && this.files[0]) {
+				var fr = new FileReader();
+				fr.onload = function(evt) {
+					$(img).attr('src', evt.target.result);
+				};
+				fr.readAsDataURL(this.files[0]);
+			}
+		});
 	}
-	this.change(function(evt){
-		 if(this.files && this.files[0]){
-	        var fr = new FileReader();
-	        fr.onload = function(evt) {
-				$(img).attr("src", evt.target.result);
-	        };       
-	        fr.readAsDataURL(this.files[0]);
-	    }
-	});
 };
-
-
