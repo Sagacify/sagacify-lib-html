dfb24feeae9ad69404424b407cf4adaa1239761a
François Beuvens <francois.beuvens@gmail.com>
2014-01-10 10:59:56 +0100

    link proto

--- prototypes/jquery/Link.js
+++ prototypes/jquery/Link.js
@@ -1,11 +1,20 @@
-$.fn.goTo = function(uri){
+$.fn.goTo = function(aliasOrRoute, args){
 	if(!this.is('a')){
 		return;
 	}
-	this.attr('href', uri);
+	var route = App.router.aliases[aliasOrRoute]!=null?App.router.aliases[aliasOrRoute]:aliasOrRoute;
+	if(args){
+		for(key in args){
+			if(route.contains(':'+key)){
+				route = route.replace(':'+key, args[key]);
+			}
+		}
+	}
+	
+	this.attr('href', route);
 	var me = this;
 	this.click(function(evt){
-		App.router.navigate(uri);
+		App.router.navigate(route);
 	});
 };
 
