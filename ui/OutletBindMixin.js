ce6c17f8fa555b57321dfc76b3f552cf6bdb8bbb
Yvan de Ghellinck <yvan@ghellinck.be>
2014-05-22 09:18:23 +0200

    action & outlet binding

---
+++ ui/OutletBindMixin.js
@@ -0,0 +1,46 @@
+define(["../model/Model"], function(Model){
+	
+	return {
+
+		outletsBind: false,
+
+
+		bindOutlets: function(){
+
+			if (!this.outletsBind) {
+				return;
+			};
+
+			var me = this;
+
+			$('[data-sgoutlet]', this.el).each(function(index){
+				var $node = $(this);
+				this.dataset["sgoutlet"+me.uid] = $node.data().sgoutlet
+				delete this.dataset["sgoutlet"];
+			});
+
+
+			$('[data-sgoutlet'+this.uid+']', this.el).each(function(index){
+				outletName = this.dataset['sgoutlet'+me.uid];
+				me.bindOutlet(this, outletName);
+			});
+
+		},
+
+		bindOutlet: function(node, outletName){
+			
+			if (this.getOutlets()[outletName]) {
+				//already binded
+				return;
+			};
+			this.getOutlets()[outletName] = $("[data-sgoutlet"+this.uid+"="+outletName+"]",this.el)
+		},
+
+		getOutlets: function(){
+			if (!this.outlets) {
+				this.outlets = {};
+			};
+			return this.outlets;
+		}
+	}
+});
