51ab9fc1d259132ad5897da4ca538777d64f9883
François Beuvens <francois.beuvens@gmail.com>
2014-06-12 09:52:30 +0200

    chart

---
+++ chart/nvd3/BarChartWithLegend.js
@@ -0,0 +1,105 @@
+define([], function(){
+	var BarChartWithLegend = function (options){
+		options = options||{};
+
+		var data = options.data||exampleData();
+		var color = nv.utils.defaultColor();
+		data[0].values.forEach(function(item, i){
+			item.color = color(item, i);
+		});
+
+		var containerSelector = options.containerSelector;
+
+		var chart = nv.models.discreteBarChart()
+			.x(function(d) { return d.label })
+			.y(function(d) { return d.value })
+			.tooltips(false)
+			.showValues(true)
+			.margin({top:50})
+
+		chart.xAxis.tickFormat("")
+		    .axisLabel("Sector"); 
+
+		var svg = d3.select(containerSelector)
+		  .datum(data); 
+
+		svg.transition().duration(500)
+		  .call(chart);
+
+		var legend = nv.models.legend();
+		legend.key(function(d) { return d.label; });
+
+		var legendSvg = svg.append("g").attr("class", "nvd3 legend");
+
+		legendSvg.datum(data[0].values) 
+			.transition()
+			.duration(500)
+		    .call(legend);
+
+		legend.dispatch.on('stateChange', function(newState) {
+			data[0].values.forEach(function(item, i){
+				item.disabled = newState.disabled[i];
+			});
+			var chartData = data.clone();
+			chartData[0].values = chartData[0].values.filter(function(item){
+				return !item.disabled;
+			});
+			console.log(data)
+			console.log(chartData)
+			svg.datum(chartData).transition().duration(500).call(chart);
+			legendSvg.datum(data[0].values).transition().duration(500).call(legend);
+			chart.update();
+		});
+
+		nv.utils.windowResize(chart.update);
+		nv.utils.windowResize(legend);
+
+		return chart;	
+	};
+
+	var exampleData = function () {
+		 return  [ 
+		    {
+		      key: "Cumulative Return",
+		      values: [
+		        { 
+		          "label" : "A Label" ,
+		          "value" : 29.765957771107
+		        } , 
+		        { 
+		          "label" : "B Label" , 
+		          "value" : 0
+		        } , 
+		        { 
+		          "label" : "C Label" , 
+		          "value" : 32.807804682612
+		        } , 
+		        { 
+		          "label" : "D Label" , 
+		          "value" : 196.45946739256,
+		        } , 
+		        { 
+		          "label" : "E Label" ,
+		          "value" : 0.19434030906893
+		        } , 
+		        { 
+		          "label" : "F Label" , 
+		          "value" : 98.079782601442
+		        } , 
+		        { 
+		          "label" : "G Label" , 
+		          "value" : 13.925743130903
+		        } , 
+		        { 
+		          "label" : "H Label" , 
+		          "value" : 5.1387322875705
+		        }
+
+		      ]
+		    }
+		  ]
+
+		}
+
+	return BarChartWithLegend;
+});
