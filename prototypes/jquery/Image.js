4080aeeba6c93f9f566ee9040889a7ab4a3b4724
Simon Debaecke <simon@sagacify.com>
2014-06-20 14:07:55 +0200

    fix cropper https://app.asana.com/0/11244388982308/12374095645673

--- prototypes/jquery/Image.js
+++ prototypes/jquery/Image.js
@@ -1,6 +1,8 @@
-$.fn.sgJcrop = function(options){
+$.fn.sgJcrop = function (options) {
 	var me = this;
-	this.Jcrop(options, function(){me.jcrop_api = this});
+	this.Jcrop(options, function () {
+		me.jcrop_api = this;
+	});
 	this.actualImgWidth = me[0].width;
 	this.actualImgHeight = me[0].height;
 	this.load(function(){
@@ -9,19 +11,19 @@ $.fn.sgJcrop = function(options){
 	});
 };
 
-$.fn.sgCrop = function(canvas){
-	if(!canvas)
+$.fn.sgCrop = function (canvas) {
+	if (!canvas) {
 		return;
+	}
 	var generatedCanvas = false;
-	if(!$(canvas).is('canvas')){
+
+	if(!$(canvas).is('canvas')) {
 		var bounds;
-		if(canvas instanceof Array){
+		if(canvas instanceof Array) {
 			bounds = canvas;
-		}
-		else if(canvas <= 1){
-			bounds = [this.actualImgWidth*canvas, this.actualImgHeight*canvas];
-		}
-		else{
+		} else if (canvas <= 1) {
+			bounds = [this.actualImgWidth * canvas, this.actualImgHeight * canvas];
+		} else {
 			bounds = [canvas, canvas];
 		}
 		canvas = document.createElement('canvas');
@@ -31,38 +33,38 @@ $.fn.sgCrop = function(canvas){
 		$('body').append(canvas);
 		generatedCanvas = true;
 	}
-	canvas = canvas[0]||canvas;
+
+	canvas = canvas[0] || canvas;
 	var context = canvas.getContext("2d");
 
-	var ratioX = this.actualImgWidth/this.width();
-	var ratioY = this.actualImgHeight/this.height();
+	var ratioX = this.actualImgWidth / this.width();
+	var ratioY = this.actualImgHeight / this.height();
 	
 	var x = this.jcrop_api.ui.selection.position().left;
 	var y = this.jcrop_api.ui.selection.position().top;
 	var width = this.jcrop_api.ui.selection.width();
 	var height = this.jcrop_api.ui.selection.height();
 
-	context.drawImage(this[0], x*ratioX, y*ratioY, width*ratioX, height*ratioY, 0, 0, $(canvas).width(), $(canvas).height());
+	context.drawImage(this[0], x * ratioX, y * ratioY, width * ratioX, height * ratioY, 0, 0, $(canvas).width(), $(canvas).height());
 	var base64 = canvas.toDataURL('image/jpeg');
-	if(generatedCanvas){
+
+	if (generatedCanvas) {
 		canvas.remove();
 	}
+
 	return base64;
 };
 
 $.fn.createNameAvatar = function (name, size) {
 	// Script by Julien Henrotte
-	var canvas = document.createElement('canvas')
-	  , ele = this.attr ? this : $(this);
+	var canvas = document.createElement('canvas'), ele = this.attr ? this : $(this);
 
 	canvas.width = (size || ele.attr('avatar-size') || ele.width() || 100) | 0;
 	canvas.height = (size || ele.attr('avatar-size') || ele.height() || 100) | 0;
 
 	var fontSize = ((canvas.width / 3) | 0) + 'px';
 
-	var firstNameInitiales
-	  , lastNameInitiales
-	  , initials = '?';
+	var firstNameInitiales, lastNameInitiales, initials = '?';
 
 	// Get the initials
 	if(name) {
@@ -110,5 +112,4 @@ $.fn.createNameAvatar = function (name, size) {
 
 	ele.attr('src', canvas.toDataURL());
 	//ele.css('background', colorMod[moduloResult]);
-
 };
