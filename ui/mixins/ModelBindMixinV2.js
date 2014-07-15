c8de07afb1833e0bdba33162f214b950c0e85d81
Simon Debaecke <simon@sagacify.com>
2014-07-15 17:08:52 +0200

    reformat

--- ui/mixins/ModelBindMixinV2.js
+++ ui/mixins/ModelBindMixinV2.js
@@ -1,10 +1,138 @@
 define([], function () {
 
+	var Binder = function (model, view, controller) {
+		this.node = view;
+		this.model = model;
+		this.controller = controller;
+
+		this.parseNodeAttribute();
+		this.bind();
+		this.tryTriggerFirstValue();
+	};
+
+	_.extend(Binder.prototype, {
+
+		// Attribute info: "change:name->.html(value)" 
+		// Attribute info: ":name->.html(value)" <=> change:name->.html(value)
+		// Attribute info: ":name" <=> change:name->.html(value)
+		// Attribute info: "change:name" <=> change:name->.html(value)
+		// Call nameToEl if present in controller
+
+		//Prepare the binder
+		parseNodeAttribute: function () {
+			var attributeInfo = $(this.node).attr('data-sgbind-' + this.controller.uid);
+			var splittedAttribute = attributeInfo.split("->");
+
+			this.trigger = null;
+			this.viewAction = null;
+			this.attribute = null;
+
+			this.trigger = splittedAttribute[0];
+
+			if (!this.trigger) {
+				throw "Error with node " + this.node;
+			}
+
+			//Syntaxic sugar :name  <=> change:name
+			if (this.trigger.startsWith(':')) {
+				this.attribute = this.trigger.replace(':', '');
+				this.trigger = 'change' + this.trigger;
+			}
+
+			if (splittedAttribute.length >= 2) {
+				//Action is define 
+				//if start with $ -> jquery eval function 
+				//Else call delegate 
+				this.viewAction = splittedAttribute[1];
+			} else {
+				//Default action
+				if (this.attribute && (this.attribute + 'ToEl' in this.controller)) {
+					//Default action if existe (call delegate)
+					this.viewAction = this.attribute + 'ToEl';
+				} else {
+					//Simple jquery html function
+					this.viewAction = '$.html(value)';
+				}
+			}
+
+			this.configureBindFunction();
+		},
+
+		isJqueryEvalAction: function () {
+			return this.viewAction.startsWith('$');
+		},
+
+		configureBindFunction: function () {
+			this._bindFunction = function (model, value, options) {
+
+				if (this.isJqueryEvalAction()) {
+					//Jquery eval function
+					var stringToApply = 'this.node' + this.viewAction.substring(1);
+					console.log(stringToApply);
+					try {
+						eval(stringToApply);
+					} catch (err) {
+						throw "Bad formated action " + this.viewAction;
+					}
+				} else {
+					//Delegate to controller
+					if (this.viewAction in this.controller) {
+						e = {
+							model: this.model,
+							value: Array.apply(null, arguments),
+							options: options,
+						};
+
+						if (this.attribute) {
+							//Back change: action
+							e.value = value;
+						}
+
+						this.controller[this.viewAction](e, this.node, this);
+					} else {
+						throw "Unknow thing to do with event " + this.getIdentifier() + " - " + this.node;
+					}
+				}
+			};
+		},
+
+		unBind: function () {
+			this.model.off(this.trigger, this._bindFunction, this);
+			this.controller = null;
+			this.node = null;
+			this.model = null;
+		},
+
+		bind: function () {
+			this.model.on(this.trigger, this._bindFunction, this);
+		},
+
+		mergeWith: function (anotherBind) {
+			this.node = $(this.node.get().concat(anotherBind.node.get()));
+		},
+
+		getIdentifier: function () {
+			return this.trigger + "-" + this.viewAction + "-" + this.model.id;
+		},
+
+		tryTriggerFirstValue: function () {
+			if (this.attribute) {
+				var value = this.model.get(this.attribute, {
+					lazyCreation: false
+				});
+				this._bindFunction.apply(this, [this.model, value]);
+			} else {
+				//unknow how to retrieve value.
+			}
+		},
+
+	});
+
 	return {
 
 		modelBindv2: false,
 
-		bindModelv2: function () {
+		__createAllBinders: function () {
 
 			if (!this.modelBindv2) {
 				return;
@@ -16,65 +144,53 @@ define([], function () {
 				throw new Error('unknow model');
 			}
 
+			//Auto generate binds
+			this.__getModelBinds();
+
 			var me = this;
 			$('[data-sgbind-' + this.uid + ']', this.el).each(function (index) {
-				attributeInfo = $(this).attr('data-sgbind-' + me.uid);
-				me.addBind($(this), attributeInfo);
+				me.__addBind(new Binder(me.model, $(this), me));
 			});
-
-			this.getModelBinds();
 		},
 
-		// Attribute info: "change:name->.html(value)" 
-		// Attribute info: ":name->.html(value)" <=> change:name->.html(value)
-		// Attribute info: ":name" <=> change:name->.html(value)
-		// Attribute info: "change:name" <=> change:name->.html(value)
-		// Call nameToEl if present in controller
-		addBind: function (node, attributeInfo) {
-			var splittedAttribute = attributeInfo.split("->");
+		__destroyAllBinder: function () {
+			if (!this.modelBindv2) {
+				return;
+			}
 
-			var trigger = null;
-			var viewAction = null;
-			var method = null;
+			_.each(this.__getModelBinds(), function (bind, identifier) {
+				bind.unBind();
+			}, this);
+			this.__getModelBinds();
+		},
 
-			if (splittedAttribute.length >= 2) {
-				viewAction = splittedAttribute[1];
+		__addBind: function (bindObj) {
+			var existSameBind = this.__getModelBind(bindObj.getIdentifier());
+
+			if (this.__getModelBind(bindObj.getIdentifier())) {
+				existSameBind.mergeWith(bindObj);
 			} else {
-				viewAction = '$.html(value)';
+				this.__getModelBinds()[bindObj.getIdentifier()] = bindObj;
 			}
-			trigger = splittedAttribute[0];
-			if (trigger.startsWith(':')) {
-				trigger = 'change' + trigger;
-			}
-
-			var me = this;
-			this.listenTo(this.model, trigger, function (listener, value, options) {
-
-				if (viewAction.startsWith('$')) {
-					var stringToApply = 'node' + viewAction.substring(1);
-					console.log(stringToApply);
-					try {
-						eval(stringToApply);
-					} catch (err) {
-						throw "Bad formated action " + viewAction;
-					}
-				} else {
-					if (me[viewAction]) {
-						return this[viewAction](value, node);
-					} else {
-						throw "Unknow thing to do with event " + this.getModelBinds()[trigger + "->" + viewAction];
-					}
-				}
-			});
-
-			this.getModelBinds()[trigger + "->" + viewAction] = node;
 		},
 
-		getModelBinds: function () {
-			if (!this._bindsV2) {
-				this._bindsV2 = {};
+		__removeBinds: function () {
+			_.each(this.__binds, function (bind) {
+				bind.destroy();
+				delete this.__binds[name];
+			}, this);
+			this.__binds = {};
+		},
+
+		__getModelBind: function (id) {
+			this.__getModelBinds()[id];
+		},
+
+		__getModelBinds: function () {
+			if (!this.__binds) {
+				this.__binds = {};
 			}
-			return this._bindsV2;
+			return this.__binds;
 		}
 	};
 });
